1547173e65ff5b25dd73a21d9720072883305a06
Ashraf266 <mohamedashrafece@gmail.com>
2024-08-23 21:08:17 +0300

    Pong _1

---
+++ Pong/src/pong/Ball.java
@@ -0,0 +1,48 @@
+package pong;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle{
+	
+	
+	Random random;
+	int xVelocity;
+	int yVelocity;
+	int speed = 10;
+	
+	public Ball(int x, int y, int diameter) {
+		super(x,y,diameter,diameter);
+		random = new Random();
+		int randomXDirection = random.nextInt(2);
+		if(randomXDirection == 0)
+			randomXDirection--;
+		setXDirection(randomXDirection);
+		
+		int randomYDirection = random.nextInt(2);
+		if(randomYDirection == 0)
+			randomYDirection--;
+		setYDirection(randomYDirection);
+	}
+	
+	public void setXDirection(int randomXDirection) {
+		xVelocity = randomXDirection*speed;
+	}
+	
+	public void setYDirection(int randomYDirection) {
+		yVelocity = randomYDirection*speed;
+	}
+	
+	public void move() {
+		x+= xVelocity;
+		y+= yVelocity;
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillOval(x, y, width, height);
+	}
+
+}
